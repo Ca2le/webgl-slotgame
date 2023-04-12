60e395927d58196e8486324ad43ebe11a09bff10
Ca2le <carl@jumpcode.org>
2023-04-12 07:33:43 +0200

    added Icon types

---
+++ src/types/Icons.ts
@@ -0,0 +1,27 @@
+export enum Icons {
+    A_10 = "../assets/asset_1.png",
+    A_J = "../assets/asset_2.png",
+    A_Q = "../assets/asset_3.png",
+    A_K = "../assets/asset_4.png",
+    A_A = "../assets/asset_5.png",
+    A_Scatter = "../assets/asset_6.png",
+    A_Wild = "../assets/asset_7.png",
+    A_Bonus = "../assets/asset_8.png",
+
+
+}
+
+export enum Model {
+    Player = "YBot",
+    NPC = "him"
+}
+
+export enum TextureId {
+    Skybox = "https://www.babylonjs-playground.com/textures/Studio_Softbox_2Umbrellas_cube_specular.env",
+    Ground = "https://www.babylonjs-playground.com/textures/sand.jpg",
+    Wall = "https://www.babylonjs-playground.com/textures/crate.png",
+    Player = "https://www.babylonjs-playground.com/scenes/dummy3.babylon",
+    NPC = "https://www.babylonjs-playground.com/scenes/Dude/dude.babylon",
+    Decal = "https://www.babylonjs-playground.com/textures/earth.jpg",
+    NormalMap = "https://www.babylonjs-playground.com/textures/floor_bump.PNG"
+}
---
+++ src/utils/slot_simulator.ts
@@ -0,0 +1,14 @@
+const reels = [
+    ['A', 'K', 'Q', 'J', 'SCATTER', 'WILD', 'BONUS'],  // reel 1
+    ['A', 'K', 'Q', 'J', 'SCATTER', 'WILD', 'BONUS'],  // reel 2
+    ['A', 'K', 'Q', 'J', 'SCATTER', 'WILD', 'BONUS'],  // reel 3
+    ['A', 'K', 'Q', 'J', 'SCATTER', 'WILD', 'BONUS'],  // reel 4
+    ['A', 'K', 'Q', 'J', 'SCATTER', 'WILD', 'BONUS']   // reel 5
+];
+
+const rows = [
+    [reels[0][0], reels[1][0], reels[2][0], reels[3][0], reels[4][0]],  // top_row
+    [reels[0][1], reels[1][1], reels[2][1], reels[3][1], reels[4][1]],  // middle_row
+    [reels[0][2], reels[1][2], reels[2][2], reels[3][2], reels[4][2]]   // bottom_row
+];
+
