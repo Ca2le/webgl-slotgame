499fa9140099dc97c9a8842a0f41e2b1cbff9708
Ca2le <carl@jumpcode.org>
2023-05-01 22:33:04 +0200

    fixed big path to asset  bug

--- src/utils/create_sprite/create_sprite.util.ts
+++ src/utils/create_sprite/create_sprite.util.ts
@@ -2,7 +2,7 @@ import { Assets } from "../../types/global.types";
 import { Sprite, utils } from "pixi.js";
 
 export function createSprite(imgIcon: Assets, width: number, height: number) {
-    const sprite = new Sprite(utils.TextureCache[`/src/assets/${imgIcon}.png`]);
+    const sprite = new Sprite(utils.TextureCache[`./assets/${imgIcon}.png`]);
     sprite.name = imgIcon
     return sprite
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    // Other plugins...
+    visualizer({
+      template: 'treemap',
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: 'bundle-analysis.html'
+    })
+  ]
+});
