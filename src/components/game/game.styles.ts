0b4f5a0324d2c382bd5dacad537c2342dec7bca1
Ca2le <carl@jumpcode.org>
2023-05-04 18:15:48 +0200

    added a aspect ratio for scaleing

--- src/components/game/game.styles.ts
+++ src/components/game/game.styles.ts
@@ -6,21 +6,5 @@ const { max } = store.getState().screenSize
 export const GameContainer = styled.div`
     width: 100%;
     height: 50vh;
-    ::after {
-        position: relative;
-
-    }
-    &::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-image: url('/assets/BACKGROUND.png');
-    background-size: cover;
-    filter: blur(4px);
-    opacity: 0.8;
-    z-index: -1;
-  }
+   
 `
